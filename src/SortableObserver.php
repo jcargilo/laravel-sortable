cbb7ee6d9b452a5584a13d935e184ef15dc74cbb
Jeremy Cargilo <jcargilo@gmail.com>
2021-09-03 11:53:39 -0400

    Handle resorting when switching between parents

--- src/SortableObserver.php
+++ src/SortableObserver.php
@@ -18,7 +18,14 @@ class SortableObserver
 
     public function updating($model): void
     {
-        if ($model->isDirty($model->getSortIndexColumn()) && $model->resortOthers) {
+        if ($model->isDirty($model->getSortingParentColumn()) && $model->resortOthers) {
+            if ($model->{$model->getSortIndexColumn()} > $model->getNextSortIndex()) {
+                $model->{$model->getSortIndexColumn()} = $model->getNextSortIndex();
+            }
+
+            $this->resortOriginalParentSortIndex($model);
+            $this->resortNewParentSortIndex($model);
+        } else if ($model->isDirty($model->getSortIndexColumn()) && $model->resortOthers) {
             if ($model->{$model->getSortIndexColumn()}> $model->getOriginal($model->getSortIndexColumn())) {
                 $this->decrementSortIndex($model);
             } else {
@@ -41,5 +48,19 @@ class SortableObserver
             ->where($model->getSortIndexColumn(), '>', $model->getOriginal($model->getSortIndexColumn()))
             ->where($model->getSortIndexColumn(), '<=', $model->{$model->getSortIndexColumn()})
             ->decrement($model->getSortIndexColumn());
+    }
+
+    private function resortOriginalParentSortIndex($model): void
+    {
+        $model->where($model->getSortingParentColumn(), '=', $model->getOriginal($model->getSortingParentColumn()))
+            ->where($model->getSortIndexColumn(), '>', $model->getOriginal($model->getSortIndexColumn()))
+            ->decrement($model->getSortIndexColumn());
+    }
+
+    private function resortNewParentSortIndex($model): void
+    {
+        $model->sameParentChild()
+            ->where($model->getSortIndexColumn(), '>=', $model->{$model->getSortIndexColumn()})
+            ->increment($model->getSortIndexColumn());
     }
 }
